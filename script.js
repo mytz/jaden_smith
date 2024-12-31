1f25b6c01abb122a7b6adffef134a91a47cb7555
mytz <98504002+mytz@users.noreply.github.com>
2024-12-31 11:51:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
             body.style.backgroundImage = "url('fondo1.png')";
             updateButtonHover(lightButton, 'LIGHTSOFF1.png', 'LIGHTSOFF2.png', 'LIGHTSOFF1.png', 'LIGHTSOFF2.png');
         } else {
-            body.style.backgroundImage = "url('fondo2.png')";
+            body.style.backgroundImage = "url('fondo3.png')";
             updateButtonHover(lightButton, 'LIGHTSON1.png', 'LIGHTSON2.png', 'LIGHTSON1.png', 'LIGHTSON2.png');
         }
         isLightOn = !isLightOn;
